e78170996a6e99b87ebde65f6faad2894f90882f
Byron Wall <Byron Wall>
2016-08-15 17:20:18 -0600

    Starts code to save TaskList

--- app/js/TaskList.js
+++ app/js/TaskList.js
@@ -20,18 +20,35 @@ module.exports = class TaskList {
         var d3 = require("d3");
         var data = this.tasks;
 
-        console.log(data);
-
         //iterates through the TaskList and creates a new DIV for each Task
+        var parent = this;
         var body = d3.select('body')
             .selectAll('div')
             .data(data).enter()
             .append('div')
             .text(function (d) { return d.description })
+            .on("click", function () {
+                parent.save();
+            })
     }
     save() {
         var jsonfile = require("jsonfile");
-        
+
+        //create new object with only the date to keep
+        var _ = require("lodash");
+        var output = _.map(this.tasks, function (d) {
+            return {
+                "desc": d.description,
+                "id": d.ID
+            }
+        })
+
+        console.log(output);
+
+        jsonfile.writeFile("./output.json", output, function (err) {
+            console.error(err);
+        })
+
     }
     load() {
         console.log("develop this");
