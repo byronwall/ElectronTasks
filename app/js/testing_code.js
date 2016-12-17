be1f4b7ffc22e901faa2e7c3f5cec2af773fb84c
Byron Wall <byron@byroni.us>
2016-12-16 22:36:09 -0700

    Pull the grid-setup into a module

--- app/js/testing_code.js
+++ app/js/testing_code.js
@@ -11,12 +11,15 @@ var jQuery = require("jquery");
 var $ = jQuery;
 
 //these requires were added to split up code.... hopefully
-var renderGrid = require("./js/grid-related.js");
+var renderGrid = require("./js/grid-render.js");
 var setupEvents = require("./js/events.js")
+
+var TaskGrid = require("./js/grid-setup.js");
+var grid;
 
 //TODO clean this section up to hide these variables
 //delcare any local/global variables
-var grid;
+
 var shouldAddTaskWhenDoneEditing = false;
 var shouldDeleteTaskWhenDoneEditing = false;
 
@@ -380,108 +383,6 @@ function clearSelection(shouldRender = true) {
     }
 }
 
-function setupGrid() {
-    grid = new EditableGrid("task-list");
-    grid.enableSort = false;
-    //TODO move these functions to their own home
-    //TODO combine these functions into a common thing, they are the same currently
-    grid.editorCancelled = function (rowIndex, columnIndex, element) {
-        //get the task for the element
-        //convert to rowId to get the correct ID in the task list
-        console.log("editor cancelled, will delete task if new")
-        var rowId = grid.getRowId(rowIndex);
-        var columnName = this.getColumnName(columnIndex);
-        var currentTask = mainTaskList.tasks[rowId];
-
-        //need to add a check here for hash tags
-        if (columnName === "description" && currentTask.description == "new task") {
-            //reset the fields before setting them again
-
-            console.log("task was deleted")
-            currentTask.removeTask();
-
-            saveTaskList(false);
-            renderGrid();
-        }
-    }
-
-    grid.editorBlurred = function (rowIndex, columnIndex, element) {
-        //get the task for the element
-        //convert to rowId to get the correct ID in the task list
-        var rowId = grid.getRowId(rowIndex);
-        var columnName = this.getColumnName(columnIndex);
-        var currentTask = mainTaskList.tasks[rowId];
-
-        //need to add a check here for hash tags
-        if (columnName === "description" && currentTask.description == "new task") {
-            //reset the fields before setting them again
-
-            console.log("task was deleted")
-            currentTask.removeTask();
-
-            saveTaskList(false);
-            renderGrid();
-        }
-
-        //TODO come up with a unified fix here
-        //this handles the disappearing comments
-        renderGrid();
-    }
-    grid.modelChanged = function (rowIndex, columnIndex, oldValue, newValue, row) {
-        //TODO update this call to handle validation
-
-        //convert to rowId to get the correct ID in the task list
-        var rowId = grid.getRowId(rowIndex);
-        var columnName = this.getColumnName(columnIndex);
-        var currentTask = mainTaskList.tasks[rowId];
-
-        //this will update the underlying data
-        currentTask[columnName] = newValue;
-        currentTask.isFirstEdit = false;
-
-        //need to add a check here for hash tags
-        if (columnName === "description") {
-            //reset the fields before setting them again
-            currentTask.tags = [];
-            currentTask.milestone = null;
-            currentTask.status = null;
-
-            //check for "#"
-            //split on space
-            var parts = newValue.split(" ");
-            var tags = [];
-            _.each(parts, function (part) {
-                switch (part[0]) {
-                    case "#":
-                        var tag = part.substring(1);
-                        tags.push(tag);
-                        break;
-                    case "@":
-                        currentTask.status = part.substring(1);
-                        break;
-                    case "!":
-                        currentTask.milestone = part.substring(1);
-                        break;
-                }
-            });
-
-            currentTask.tags = tags;
-        }
-
-        saveTaskList();
-        renderGrid();
-
-        if (shouldAddTaskWhenDoneEditing && columnName === "description") {
-            var options = {
-                parentTask: currentTask.parentTask,
-                sortOrder: currentTask.sortOrder + 0.5
-            }
-            createNewTask(options);
-            shouldAddTaskWhenDoneEditing = false;
-        }
-    };
-}
-
 function setupLocalStorage() {
     //load the recentfile list from localStorage
     recentFiles = JSON.parse(localStorage.getItem("recentFiles"));
@@ -585,7 +486,9 @@ function setupMainPageTasks() {
     mainTaskList = new TaskList();
 
     //set up the grid related events
-    setupGrid();
+    //TODO remove this hack    
+    grid = (new TaskGrid()).getGrid();
+
     setupLocalStorage();
     createColumnShowAndSort();
 
