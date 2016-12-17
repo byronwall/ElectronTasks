b187fc6a9afd33ed498cf24de8069de70b4a1cb9
Byron Wall <byron@byroni.us>
2016-12-16 22:45:43 -0700

    Move local-storage to module

--- app/js/testing_code.js
+++ app/js/testing_code.js
@@ -17,6 +17,8 @@ var setupEvents = require("./js/events.js")
 var TaskGrid = require("./js/grid-setup.js");
 var grid;
 
+var setupLocalStorage = require("./js/local-storage.js");
+
 //TODO clean this section up to hide these variables
 //delcare any local/global variables
 
@@ -380,20 +382,6 @@ function clearSelection(shouldRender = true) {
     })
     if (shouldRender) {
         renderGrid();
-    }
-}
-
-function setupLocalStorage() {
-    //load the recentfile list from localStorage
-    recentFiles = JSON.parse(localStorage.getItem("recentFiles"));
-    if (recentFiles == null) {
-        recentFiles = [];
-    }
-    updateRecentFileButton();
-
-    visibleColumns = JSON.parse(localStorage.getItem("visibleColumns"));
-    if (visibleColumns == null) {
-        visibleColumns = ["description"];
     }
 }
 
